3c8a2ea166485a378602436d30b0b067f0efc02f
jshiah <joyce.shiah@gmail.com>
2024-10-25 14:03:44 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
 <main>
     <h1>Joyce Shiah</h1>
     <p>MS DS student @ UCSD HDSI. Welcome to my Lab-1 Assignment.</p>
-    <img src="/freezercat.jpeg" alt="I saw a cat named Freezer at the cat shelter." width = "500" height = "700"/>
+    <img src="/my-portfolio/freezercat.jpeg" alt="I saw a cat named Freezer at the cat shelter." width = "500" height = "700"/>
 </main>

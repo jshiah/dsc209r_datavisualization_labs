1cf559257fe0706a275a2d2ca7302384f2a161f2
jshiah <joyce.shiah@gmail.com>
2024-11-01 14:38:30 -0700

    edits

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,33 @@
+// import adapter from '@sveltejs/adapter-static';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	kit: {
+// 		adapter: adapter({
+// 			fallback: '404.html'
+// 		}),
+// 		paths: {
+// 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+// 		}
+// 	}
+// };
+
+// export default config;
+
+
 import adapter from '@sveltejs/adapter-static';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+    kit: {
+        adapter: adapter({
+            // default options are shown. On some environments, these options are set automatically
+            pages: 'build',
+            assets: 'build',
+        }),
+        paths: {
+            base: process.env.BASE_PATH || ''
+        }
+    }
 };
 
 export default config;
